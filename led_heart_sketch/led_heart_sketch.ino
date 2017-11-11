155558292e107854ec5739c762b84c4d2b9247f1
proletarius <s.kulchanovskiy@gmail.com>
2017-11-11 15:11:07 +0600

    Graphic messages display addeed

--- led_heart_sketch/led_heart_sketch.ino
+++ led_heart_sketch/led_heart_sketch.ino
@@ -1,14 +1,54 @@
 //  @autor = s.kulchanovskiy@gmail.com
 //  @microcontroller = Atmega328p 
 
+#include <OLED_I2C.h> //импорт библиотеки
+OLED  myOLED(SDA, SCL, 8); //создаем экземпляр класса OLED с именем myOLED
+extern uint8_t logo[];
+extern uint8_t nelya[];
+extern uint8_t message[];
+extern uint8_t family[];
 
+void display_logo(int pause){
+    myOLED.clrScr();
+    myOLED.drawBitmap(0, 0, logo, 128, 64);
+    myOLED.update();
+    delay(pause);
+    for (int i=0; i<2; i++)
+    {
+      myOLED.invert(true);
+      delay(500);
+      myOLED.invert(false);
+      delay(500);
+    }
+    delay(pause);
+  }
+
+void display_message(int pause){
+    myOLED.clrScr();
+    myOLED.drawBitmap(0, 0, nelya, 128, 64);
+    myOLED.update();
+    delay(500);
+    myOLED.invert(true);
+    delay(pause); 
+    myOLED.clrScr();
+    myOLED.drawBitmap(0, 0, message, 128, 64);
+    myOLED.update();
+    delay(pause);
+    
+    myOLED.clrScr();
+    myOLED.drawBitmap(0, 0, family, 128, 64);
+    myOLED.update();
+    delay(pause);
+    myOLED.invert(false);
+    delay(500);
+  }
 
 void setup() {
-  // put your setup code here, to run once:
-
-}
+    myOLED.begin(); //инициализируем дисплей
+    myOLED.setBrightness(10); //настраиваем яркость дисплея
+  }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+    display_logo(2000);
+    display_message(2000);
+  }
