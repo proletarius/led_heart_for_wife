57a899b869ae81f55691f6e5d7e4df3590e18f24
proletarius <s.kulchanovskiy@gmail.com>
2017-11-11 23:06:36 +0600

    Animated flowers added

--- led_heart_sketch/led_heart_sketch.ino
+++ led_heart_sketch/led_heart_sketch.ino
@@ -7,9 +7,25 @@ extern uint8_t logo[];
 extern uint8_t nelya[];
 extern uint8_t message[];
 extern uint8_t family[];
+extern uint8_t flower[];
 
 #define FLINE1_LED_PIN  2
 #define LLINE1_LED_PIN  13
+
+void animate_flower(){
+    for(int i = -50; i<70; i+=2){
+        myOLED.clrScr();
+        myOLED.drawBitmap(0, i, flower, 50, 50);
+        myOLED.drawBitmap(78, -i, flower, 50, 50);
+        myOLED.update();
+      }
+    for(int i = -50; i<70; i+=2){
+        myOLED.clrScr();
+        myOLED.drawBitmap(i, i, flower, 50, 50);
+        myOLED.drawBitmap(78, -i, flower, 50, 50);
+        myOLED.update();
+      }
+  }
 
 void display_logo(int pause){
     myOLED.clrScr();
@@ -26,23 +42,28 @@ void display_logo(int pause){
     delay(pause);
   }
 
-void display_message(int pause){
+void display_message_1(int pause){
     myOLED.clrScr();
     myOLED.drawBitmap(0, 0, nelya, 128, 64);
     myOLED.update();
     delay(500);
     myOLED.invert(true);
-    delay(pause);
+    delay(pause);  
+  } 
+
+void display_message_2(int pause){
     myOLED.clrScr();
     myOLED.drawBitmap(0, 0, message, 128, 64);
     myOLED.update();
-    delay(pause);
+    delay(pause);  
+  }
+
+void display_message_3(int pause){
     myOLED.clrScr();
     myOLED.drawBitmap(0, 0, family, 128, 64);
     myOLED.update();
     delay(pause);
     myOLED.invert(false);
-    delay(500);
   }
 
 void led_on(){
@@ -130,23 +151,31 @@ void setup() {
 void loop() {
     led_off();
     led_random();
+    animate_flower();
     for(int i = 1; i<=3; ++i){
         led_on();
-        delay(500);
+        delay(50);
         led_off();
-        delay(500);
+        delay(50);
       }
     led_random();   
     led_effect_1(50);
     led_random();
-    display_logo(1000);
+    display_logo(500);
     led_random();
     for(int i=1; i<=3; ++i){
         led_effect_2(50);
     }
-    led_random();    
-    display_message(1000);
     led_random();
+        
+    display_message_1(500);
+    led_random();
+    led_effect_4(50);
+    display_message_2(500);
+    led_random();
+    led_effect_1(50);
+    display_message_3(500);
+    led_random();  
     led_effect_3(50);
     led_random();
     for(int i=1; i<=3; ++i){
