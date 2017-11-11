629811f9c28560fac08e555a0b0ea0534e705219
proletarius <s.kulchanovskiy@gmail.com>
2017-11-11 21:32:35 +0600

    Random leds added

--- led_heart_sketch/led_heart_sketch.ino
+++ led_heart_sketch/led_heart_sketch.ino
@@ -62,7 +62,8 @@ void led_effect_1(int pause){
       digitalWrite(pin, HIGH);
       delay(pause);
       digitalWrite(pin, LOW);
-    }  
+    }
+    led_random();
   }
 
 void led_effect_2(int pause){
@@ -71,11 +72,13 @@ void led_effect_2(int pause){
       digitalWrite(pin+6, HIGH);
       delay(pause);
     }
+    led_random();
   for (int pin = FLINE1_LED_PIN; pin <= 7; ++pin){
       digitalWrite(pin, LOW);
       digitalWrite(pin+6, LOW);
       delay(pause);
     }
+   led_random();
   }
 
 void led_effect_3(int pause){
@@ -83,12 +86,12 @@ void led_effect_3(int pause){
       digitalWrite(pin, HIGH);
       delay(pause);
     }
-  
+  led_random();
   for (int pin = FLINE1_LED_PIN; pin <= LLINE1_LED_PIN; ++pin){
       digitalWrite(pin, LOW);
       delay(pause);
     }
-        
+  led_random();     
   }
 
 void led_effect_4(int pause){
@@ -97,40 +100,57 @@ void led_effect_4(int pause){
       digitalWrite(pin+6, HIGH);
       delay(pause);
     }
+  led_random();
   for (int pin = 7; pin >= FLINE1_LED_PIN; --pin){
       digitalWrite(pin, LOW);
       digitalWrite(pin+6, LOW);
       delay(pause);
-    }  
+    }
+  led_random();  
   }
 
+void led_random(){
+  int p = random(14, 18);
+  if(digitalRead(p) == HIGH){
+      digitalWrite(p,LOW);
+    }else{
+        digitalWrite(p,HIGH);
+      }
+  }
 
 void setup() {
+    randomSeed(analogRead(0));
     myOLED.begin(); //инициализируем дисплей
     myOLED.setBrightness(10); //настраиваем яркость дисплея
-    for (int pin = FLINE1_LED_PIN; pin <= LLINE1_LED_PIN; ++pin){
+    for (int pin = FLINE1_LED_PIN; pin <= LLINE1_LED_PIN+4; ++pin){
         pinMode(pin, OUTPUT);
       }
-    
   }
 
 void loop() {
     led_off();
+    led_random();
     for(int i = 1; i<=3; ++i){
         led_on();
         delay(500);
         led_off();
         delay(500);
-      }   
+      }
+    led_random();   
     led_effect_1(50);
+    led_random();
     display_logo(1000);
-
+    led_random();
     for(int i=1; i<=3; ++i){
         led_effect_2(50);
-      }    
+    }
+    led_random();    
     display_message(1000);
+    led_random();
     led_effect_3(50);
+    led_random();
     for(int i=1; i<=3; ++i){
         led_effect_4(50);
       }
+    led_random();
   }
