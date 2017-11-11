84e26647222ece05ed082f92e529db926c154d2e
proletarius <s.kulchanovskiy@gmail.com>
2017-11-11 13:23:12 +0600

    Arduino sketch created

---
+++ led_heart_sketch/led_heart_sketch.ino
@@ -0,0 +1,14 @@
+//  @autor = s.kulchanovskiy@gmail.com
+//  @microcontroller = Atmega328p 
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
